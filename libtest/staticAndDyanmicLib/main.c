d2478d7aed2311e7f06a285b149029e6dbc2a39b
Craig Yang <craig.yang@hpe.com>
2017-11-20 17:03:58 +0800

    This example shows how to complie static, shared (*.a) and dynamic (*.so) library

--- libtest/staticAndDyanmicLib/main.c
+++ libtest/staticAndDyanmicLib/main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #if defined(STATIC)||defined (IMPLICITDYNAMIC)
 #include "libmyadd.h"
-#endif 
+#endif
 #if defined (EXPLICITDYNAMIC)
 #include<dlfcn.h>
 #endif
@@ -11,41 +11,26 @@
 int main(int *argc, char **argv)
 {
 #if defined(STATIC)||defined (IMPLICITDYNAMIC)
-	printf("Result = %d\n",myadd(10,20));
+    printf("Result = %d\n",myadd(10,20));
 #endif
 
 #if defined (EXPLICITDYNAMIC)
-	void    *fHandle;
-	int     (*func)(int, int); // 注意傳入參數和回傳參數的型態
-	fHandle = dlopen("./libmyadd.so",RTLD_LAZY);
-	if (!fHandle) {
-		fprintf (stderr, "%s\n", dlerror());
-		exit(1);
+    void    *fHandle;
+    int     (*func)(int, int); // 注意傳入參數和回傳參數的型態
+    fHandle = dlopen("./libmyadd.so",RTLD_LAZY);
+    if (!fHandle) {
+        fprintf (stderr, "%s\n", dlerror());
+        exit(1);
 
-	}
-	dlerror();
-	func = dlsym(fHandle, "myadd");
+    }
+    dlerror();
+    func = dlsym(fHandle, "myadd");
 
-	if (func) {        
-		printf("Result = %d\n",func(10,20));
-	}
-	dlclose(fHandle);
+    if (func) {
+        printf("Result = %d\n",func(10,20));
+    }
+    dlclose(fHandle);
 
-/*
-	fHandle = dlopen("./libanother.so",RTLD_LAZY);
-	if (!fHandle) {
-		fprintf (stderr, "%s\n", dlerror());
-		exit(1);
-
-	}
-	dlerror();
-	func = dlsym(fHandle, "myadd");
-
-	if (func) {        
-		printf("Result = %d\n",func(10,20));
-	}
-	dlclose(fHandle);
-*/
 #endif
-	return 0;
+    return 0;
 }
